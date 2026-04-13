b93f31ffa7d519af4c1a40f8019aac782e0fba5f
DavidSwims <david.gerchik@gmail.com>
2026-04-12 20:48:30 -0500

    Implement hello and starts_with_consonant? methods in ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -36,15 +36,16 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.nil? || s.empty?
+  s[0] =~ /[^aeiou]/i && s[0] =~ /[a-zA-Z]/
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #code
 end
 
 # Part 3
